be04ebc5bf07851e22512f3fd020ae5e3cc32e20
Harald Skoglund <haraldsk@redpill-linpro.com>
2012-11-17 13:36:52 +0100

    More tests.

--- spec/classes/client_redhat_spec.rb
+++ spec/classes/client_redhat_spec.rb
@@ -1,8 +1,11 @@
 require 'spec_helper'
 describe 'nfs::client::redhat' do
-
-  let(:facts) { {:operatingsystem => 'redhat', :osmajor => 6 } }
+  let(:facts) { {:osmajor => 6 } }
   it do
+    should include_class('nfs::client::redhat::install')
+    should include_class('nfs::client::redhat::configure')
+    should include_class('nfs::client::redhat::service')
+
     should contain_service('nfslock').with(
       'ensure' => 'running'
     )
@@ -10,19 +13,22 @@ describe 'nfs::client::redhat' do
       'enable' => 'true'
     )
     should contain_package('nfs-utils')
+    should include_class('nfs::client::redhat')
+    should contain_package('rpcbind')
+    should contain_service('rpcbind').with(
+      'ensure' => 'running'
+    )
   end
-  context "osmajor => 6" do
-    let(:facts) { {:operatingsystem => 'redhat', :osmajor => 6 } }
+
+  context ":nfs_v4 => true" do
+    let(:params) {{ :nfs_v4 => true }}
     it do
-      should include_class('nfs::client::redhat')
-      should contain_package('rpcbind')
-      should contain_service('rpcbind').with(
-        'ensure' => 'running'
-      )
+      should contain_augeas('/etc/idmapd.conf') 
     end
   end
+
   context "osmajor => 5" do
-    let(:facts) { {:operatingsystem => 'redhat', :osmajor => 5 } }
+    let(:facts) { {:osmajor => 5 } }
     it do
       should include_class('nfs::client::redhat')
       should contain_package('portmap')
