46db73f234a676aa7adf71336f6415a38961812f
Min Pae <sputnik13@gmail.com>
2013-11-20 07:03:59 -0800

    adding anchor pattern to nfs client and dependency on nfs::client to mount call to avoid race condition with nfs services not starting before the mount call is made

--- manifests/client/redhat.pp
+++ manifests/client/redhat.pp
@@ -6,9 +6,18 @@ class nfs::client::redhat (
   $nfs_v4_idmap_domain = undef
 ) inherits nfs::client::redhat::params {
 
-  include nfs::client::redhat::install, 
-    nfs::client::redhat::configure, 
+  include nfs::client::redhat::install,
+    nfs::client::redhat::configure,
     nfs::client::redhat::service
 
+  anchor{ 'nfs::client::redhat::start': }
+  ->
+  Class['nfs::client::redhat::install']
+  ->
+  Class['nfs::client::redhat::configure']
+  ->
+  Class['nfs::client::redhat::service']
+  ->
+  anchor{ 'nfs::client::redhat::end': }
 
 }
