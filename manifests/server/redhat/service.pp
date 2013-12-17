3c62ef6d52269ac6f023a05a2facbdc79aa7498d
Min Pae <sputnik13@s13-mba.sputnik13.net>
2013-12-16 22:53:14 -0800

    adding support for firewall module for redhat based systems

--- manifests/server/redhat/service.pp
+++ manifests/server/redhat/service.pp
@@ -1,5 +1,20 @@
 class nfs::server::redhat::service {
 
+  if defined(Class['firewall']) {
+    augeas { 'nfs config':
+      context => '/files',
+      changes => [
+        "set etc/sysconfig/nfs/rquotad_port ${nfs::server::redhat::rquotad_port}",
+        "set etc/sysconfig/nfs/lockd_tcpport ${nfs::server::redhat::lockd_tcpport}",
+        "set etc/sysconfig/nfs/lockd_udpport ${nfs::server::redhat::lockd_udpport}",
+        "set etc/sysconfig/nfs/mountd_port ${nfs::server::redhat::mountd_port}",
+        "set etc/sysconfig/nfs/statd_port ${nfs::server::redhat::statd_port}",
+        "set etc/sysconfig/nfs/statd_outgoing_port ${nfs::server::redhat::statd_outgoing_port}",
+      ],
+      notify => Service['nfs'],
+    }
+  }
+
   if $nfs::server::redhat::nfs_v4 == true {
       service {"nfs":
         ensure     => running,
