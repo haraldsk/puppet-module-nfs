b5bf1565940acc1028b04769bb0d088acb5b16cb
Matthias Viehweger <kronn@kronn.de>
2014-01-14 13:54:03 +0100

    Resolve deprecation warnings by puppet-rspec

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -15,29 +15,29 @@ describe 'nfs::server' do
 
   context "operatingsysten => ubuntu" do
     let(:facts) { {:operatingsystem => 'ubuntu', :concat_basedir => '/tmp', } }
-    it { should include_class('nfs::server::debian') }
+    it { should contain_class('nfs::server::debian') }
   end
   context "operatingsysten => debian" do
     let(:facts) { {:operatingsystem => 'debian', :concat_basedir => '/tmp',} }
-    it { should include_class('nfs::server::debian') }
+    it { should contain_class('nfs::server::debian') }
   end
   context "operatingsysten => scientific" do
     let(:facts) { {:operatingsystem => 'scientific', :concat_basedir => '/tmp', :osmajor => 6, } }
-    it { should include_class('nfs::server::redhat') }
+    it { should contain_class('nfs::server::redhat') }
   end
   context "operatingsysten => centos v6" do
     let(:facts) { {:operatingsystem => 'centos', :concat_basedir => '/tmp', :osmajor => 6, } }
-    it { should include_class('nfs::server::redhat') }
+    it { should contain_class('nfs::server::redhat') }
   end
   context "operatingsysten => redhat v6" do
     let(:facts) { {:operatingsystem => 'redhat', :concat_basedir => '/tmp', :osmajor => 6, } }
-    it { should include_class('nfs::server::redhat') }
+    it { should contain_class('nfs::server::redhat') }
   end
   context "operatingsysten => darwin" do
     let(:facts) { {:operatingsystem => 'darwin'} }
     it do
       expect {
-        should include_class('nfs::server::darwin')
+        should contain_class('nfs::server::darwin')
       }.to raise_error(Puppet::Error, /NFS server is not supported on Darwin/)
     end
   end
