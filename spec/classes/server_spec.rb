e0033c97d61fe069f7208470b2092bf1b7c28498
Matthias Viehweger <kronn@kronn.de>
2014-01-14 15:56:31 +0100

    Extract common OS-specific behaviour

--- spec/classes/server_spec.rb
+++ spec/classes/server_spec.rb
@@ -11,27 +11,35 @@ describe 'nfs::server' do
       should contain_concat__fragment('nfs_exports_root').with( 'target' => '/etc/exports' )
       should contain_file('/export').with( 'ensure' => 'directory' )
     end
-    end
+  end
+
+  shared_examples :debian do
+    it { should contain_class('nfs::server::debian') }
+  end
+
+  shared_examples :redhat do
+    it { should contain_class('nfs::server::redhat') }
+  end
 
   context "operatingsysten => ubuntu" do
     let(:facts) { {:operatingsystem => 'ubuntu', :concat_basedir => '/tmp', } }
-    it { should contain_class('nfs::server::debian') }
+    it_behaves_like :debian
   end
   context "operatingsysten => debian" do
     let(:facts) { {:operatingsystem => 'debian', :concat_basedir => '/tmp',} }
-    it { should contain_class('nfs::server::debian') }
+    it_behaves_like :debian
   end
   context "operatingsysten => scientific" do
     let(:facts) { {:operatingsystem => 'scientific', :concat_basedir => '/tmp', :osmajor => 6, } }
-    it { should contain_class('nfs::server::redhat') }
+    it_behaves_like :redhat
   end
   context "operatingsysten => centos v6" do
     let(:facts) { {:operatingsystem => 'centos', :concat_basedir => '/tmp', :osmajor => 6, } }
-    it { should contain_class('nfs::server::redhat') }
+    it_behaves_like :redhat
   end
   context "operatingsysten => redhat v6" do
     let(:facts) { {:operatingsystem => 'redhat', :concat_basedir => '/tmp', :osmajor => 6, } }
-    it { should contain_class('nfs::server::redhat') }
+    it_behaves_like :redhat
   end
   context "operatingsysten => darwin" do
     let(:facts) { {:operatingsystem => 'darwin'} }
