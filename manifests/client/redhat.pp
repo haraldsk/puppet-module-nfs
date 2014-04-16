c66a51ca9c4ff6e88e9a7e6a21e423d89dfa8f09
Vladislav Nazarenko <vladislav.nazarenko@dvag.com>
2014-04-16 12:58:32 +0200

    upd: nfs::client now includes nfs::client:${osfamily}, needed to install both server and client on the same node

--- manifests/client/redhat.pp
+++ manifests/client/redhat.pp
@@ -2,13 +2,11 @@
 # refactored a bit
 
 class nfs::client::redhat (
-  $nfs_v4 = false,
-  $nfs_v4_idmap_domain = undef
+  $nfs_v4              =  $::nfs::client::nfs_v4,
+  $nfs_v4_idmap_domain =  $::nfs::client::nfs_v4_idmap_domain,
 ) inherits nfs::client::redhat::params {
 
-  include nfs::client::redhat::install, 
-    nfs::client::redhat::configure, 
+  include nfs::client::redhat::install,
+    nfs::client::redhat::configure,
     nfs::client::redhat::service
-
-
 }
