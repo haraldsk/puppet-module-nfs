39696460dd48e737cacc9c2180582c559cef78a5
root <root@informatik.haw-hamburg.de>
2014-04-24 14:30:20 +0200

    ubuntusupport removed, only debian

--- manifests/client/debian/install.pp
+++ manifests/client/debian/install.pp
@@ -1,22 +1,11 @@
 class nfs::client::debian::install {
 
-  case $::operatingsystem  {
-    'Ubuntu': {     # Valid for version after lucid 
-      package { 'portmap':
-        ensure => installed,
-      }
-
-     Package['portmap'] ->  Service ['portmap']
-
-    } default: {
       package { 'rpcbind':
         ensure => installed,
       }
 
      Package['rpcbind'] -> Service ['portmap']
 
-    }
-  }
 
   package { ['nfs-common', 'nfs4-acl-tools']:
     ensure => installed,
