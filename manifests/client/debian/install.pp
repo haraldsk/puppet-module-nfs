f0cda4a950577d5fe9520a8287d22d9f0fba72ef
Peter Radzuweit <pera@informatik.haw-hamburg.de>
2014-05-06 11:50:03 +0200

    cleanup package selection, drop support for ubuntu <= lucid(10.04)

--- manifests/client/debian/install.pp
+++ manifests/client/debian/install.pp
@@ -1,22 +1,12 @@
 class nfs::client::debian::install {
 
-  case $::operatingsystem  {
-    'Ubuntu': {     # Valid for version after lucid 
-      package { 'portmap':
-        ensure => installed,
-      }
-
-     Package['portmap'] ->  Service ['portmap']
-
-    } default: {
+  
       package { 'rpcbind':
         ensure => installed,
       }
 
-     Package['rpcbind'] -> Service ['portmap']
+     Package['rpcbind'] -> Service ['rpcbind']
 
-    }
-  }
 
   package { ['nfs-common', 'nfs4-acl-tools']:
     ensure => installed,
