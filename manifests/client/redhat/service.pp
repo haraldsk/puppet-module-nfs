bc4cb83959ca65d3af5f9079f134ae3a179ddc44
falcas <falcas@optymyze.com>
2014-09-09 18:21:09 +0300

    add el7 support. nfslock is now nfs-lock. Also, netfs service doesn't exist.

--- manifests/client/redhat/service.pp
+++ manifests/client/redhat/service.pp
@@ -2,42 +2,47 @@
 # refactored a bit
 
 class nfs::client::redhat::service {
-
   Service {
-    require => Class['nfs::client::redhat::configure']
+    require => Class['nfs::client::redhat::configure'] }
+
+  if $::operatingsystemmajrelease == 7 {
+    $nfslock_service = 'nfs-lock'
+  } else {
+    $nfslock_service = 'nfslock'
+
+    service { "netfs":
+      enable  => true,
+      require => $nfs::client::redhat::osmajor ? {
+        6 => Service[$nfslock_service],
+        5 => [Service["portmap"], Service[$nfslock_service]],
+      },
+    }
   }
 
-  service {"nfslock":
-    ensure     => running,
+  service { $nfslock_service:
+    ensure    => running,
     enable    => true,
     hasstatus => true,
-    require => $nfs::client::redhat::osmajor ? {
+    require   => $nfs::client::redhat::osmajor ? {
       6 => Service["rpcbind"],
       5 => [Package["portmap"], Package["nfs-utils"]]
     },
   }
 
-  service { "netfs":
-    enable  => true,
-    require => $nfs::client::redhat::osmajor ? {
-      6 => Service["nfslock"],
-      5 => [Service["portmap"], Service["nfslock"]],
-    },
-  }
-
   if $nfs::client::redhat::osmajor == 6 {
-    service {"rpcbind":
+    service { "rpcbind":
       ensure    => running,
       enable    => true,
       hasstatus => true,
-      require => [Package["rpcbind"], Package["nfs-utils"]],
+      require   => [Package["rpcbind"], Package["nfs-utils"]],
     }
   } elsif $nfs::client::redhat::osmajor == 5 {
     service { "portmap":
       ensure    => running,
       enable    => true,
       hasstatus => true,
-      require => [Package["portmap"], Package["nfs-utils"]],
+      require   => [Package["portmap"], Package["nfs-utils"]],
     }
   }
 }
+
